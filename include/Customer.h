dd5b3ffd75f7892e0902a82ba99479f4b46931c5
Emanuela Asiedu <emanuela.asiedu@chasacademy.se>
2024-12-09 09:30:11 +0100

    cusstomer fil

---
+++ include/Customer.h
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+#include "User.h"
+#include "AlarmSystem.h"
+class Customer
+{
+private:
+   int customerId;
+   std::string name;
+   
+public:
+std::string adress;
+
+  void addUser(User : user){
+
+  } 
+  void addAlarmSystem ( Alarmsystem : alarm){
+
+  }
+};
+
+
+
+
---
+++ src/Customer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string.h>
+#include "User.h"
+#include "AlarmSystem.h"
+#include "Customer.h"
+
+class Customer {
+private:
+    int customerId;
+    std::string name;
+
+public:
+    std::string address;
+
+    void addUser(User user) {
+        // Implementation for adding a user
+    }
+
+    void addAlarmSystem(AlarmSystem alarm) {
+        // Implementation for adding an alarm system
+    }
+};
+
+int main() {
+    Customer customer;
+    
+
+    User user;
+    // Initialize user object
+
+    AlarmSystem alarm;
+    // Initialize alarm system object
+
+
+
+    return 0;
+}
