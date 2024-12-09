03cb6b06955902bd595686c8fa8ce7e6c4d28471
robag1137 <robert.bagunda@chasacademy.se>
2024-12-09 09:51:25 +0100

    Added User

---
+++ include/User.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string.h>
+
+class User
+{
+private:
+    int userId;
+    std::string code;
+    std::string tagld;
+    std::string verificationPhase;
+public:
+    bool verifyAlarm; 
+};
+
+
---
+++ src/User.cpp
@@ -0,0 +1,5 @@
+#include "User.h" 
+#include <iostream>
+
+User::User() : userId(0), verifyAlarm() {}
+
